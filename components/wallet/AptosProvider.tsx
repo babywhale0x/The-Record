7ad30fd76f7c4921e50547a1a474f628ce67c5c0
0xbabywhale <0xbabywhale5000@gmail.com>
2026-03-15 23:37:32 +0100

    fix: add dappConfig to AptosProvider, use adapter connect for Aptos Connect

--- components/wallet/AptosProvider.tsx
+++ components/wallet/AptosProvider.tsx
@@ -1,12 +1,20 @@
 'use client'
 
 import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react'
+import { Network } from '@aptos-labs/ts-sdk'
 import { ReactNode } from 'react'
 
 export default function AptosProvider({ children }: { children: ReactNode }) {
   return (
     <AptosWalletAdapterProvider
       autoConnect={true}
+      dappConfig={{
+        network: Network.TESTNET,
+        aptosConnectDappId: 'the-record-dapp',
+        mizuwallet: {
+          manifestURL: 'https://assets.mz.xyz/static/config/mizuwallet-connect-manifest.json',
+        },
+      }}
       onError={(error) => {
         console.error('Wallet error:', error)
       }}
